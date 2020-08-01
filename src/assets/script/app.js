9a737486ee9b89505d17fa92d4d11a23fb0ac617
iproman <itechman@yandex.ru>
2020-08-01 11:55:42 +0500

    Get input values by setup events from UIController.

--- src/assets/script/app.js
+++ src/assets/script/app.js
@@ -18,6 +18,13 @@ var UIController = (function () {
 
     // Some code
     return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            }
+        },
         getDOMStrings: function () {
             return DOMStrings;
         }
@@ -32,16 +39,28 @@ var controller = (function (budgetCtrl, UICtrl) {
 
         let DOM = UICtrl.getDOMStrings();
 
-        document.querySelector(DOM.inputBtn).addEventListener('click', function () {
-        });
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
         document.addEventListener('keypress', function (event) {
 
             // Enter and numpadEnter
             if (event.key === 'Enter') {
-
+                ctrlAddItem();
             }
         })
+    }
+
+    var ctrlAddItem = function () {
+        // 1. get input data
+        var input = UICtrl.getInput();
+
+        // 2. add time to bg ctrl
+
+        // 3. add new item to ui
+
+        // 4. calc bdg
+
+        // 5. display bdg
     }
 
     return {
