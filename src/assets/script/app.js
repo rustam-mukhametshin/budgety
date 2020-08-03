2a8fcba5c03ff1188a1be696fd4eee3f8dfcdaf2
iproman <itechman@yandex.ru>
2020-08-03 09:17:39 +0500

    Html templates based on type of input.

--- src/assets/script/app.js
+++ src/assets/script/app.js
@@ -79,13 +79,38 @@ var UIController = (function () {
             }
         },
         addListItem: function (obj, type) {
-            let element;
+            let html, element;
 
             // Create HTML string with placeholder text
             if (type === 'inc') {
                 element = DOMStrings.incomeContainer;
+
+                html = `
+                    <div class="item clearfix" id="income-%id%">
+                        <div class="item__description">%description%</div>
+                        <div class="right clearfix">
+                            <div class="item__value">+ %value%</div>
+                            <div class="item__delete">
+                                <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                `;
             } else if (type === 'exp') {
                 element = DOMStrings.expensesContainer;
+
+                html = `
+                  <div class="item clearfix" id="expense-%id%">
+                        <div class="item__description">%description%</div>
+                        <div class="right clearfix">
+                            <div class="item__value">- %value%</div>
+                            <div class="item__percentage">21%</div>
+                            <div class="item__delete">
+                                <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                            </div>
+                        </div>
+                    </div>
+             `;
             }
 
         },
