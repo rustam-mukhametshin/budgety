9664ae6e5ba7fbfade241fb0cd405077f277cd0a
iproman <itechman@yandex.ru>
2020-08-07 10:01:26 +0500

    Change html templates for items.

--- src/assets/script/app.js
+++ src/assets/script/app.js
@@ -187,32 +187,16 @@ let UIController = (function () {
             if (type === 'inc') {
                 element = DOMStrings.incomeContainer;
 
-                html = `
-                    <div class="item clearfix" id="inc-%id%">
-                        <div class="item__description">%description%</div>
-                        <div class="right clearfix">
-                            <div class="item__value">+ %value%</div>
-                            <div class="item__delete">
-                                <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                `;
+                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div>' +
+                    '<div class="right clearfix"><div class="item__value">+ %value%</div><div class="item__delete">' +
+                    '<button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if (type === 'exp') {
                 element = DOMStrings.expensesContainer;
 
-                html = `
-                  <div class="item clearfix" id="exp-%id%">
-                        <div class="item__description">%description%</div>
-                        <div class="right clearfix">
-                            <div class="item__value">- %value%</div>
-                            <div class="item__percentage">21%</div>
-                            <div class="item__delete">
-                                <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
-                            </div>
-                        </div>
-                    </div>
-             `;
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div>' +
+                    '<div class="right clearfix"><div class="item__value">- %value%</div>' +
+                    '<div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">' +
+                    '<i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
             }
 
             // Replace the placeholder text with some actual data
